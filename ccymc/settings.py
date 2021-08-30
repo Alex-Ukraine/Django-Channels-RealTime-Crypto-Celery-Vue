d487d491de3dd06aab59829d10e2844474935fba
oleksii_esaulov <oleksii_esaulov@epam.com>
2021-08-30 22:33:31 +0300

    secret_key hidden

--- ccymc/settings.py
+++ ccymc/settings.py
@@ -22,7 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-03jwnhz@#-80x)2ah0k!fk@s*ubj45v0vkq&dz!^_#&4dt#_vj'
+try:
+    from .secret import SECRET_KEY
+except:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
